7089812e6d2d2b156d19fb6ece90c2d6a3bc3e3d
o.zanganeh@gmail.com <ozangane@AD.MONASH.EDU>
2016-02-07 12:20:59 +1100

    Q2-incode comments added.

--- counter.php
+++ counter.php
@@ -2,12 +2,16 @@
 
 for ($i = 1; $i <= 100; $i++) {
     echo "$i ";
+//this if checkes if the number is divisible to 3	
 if ( $i % 3 == 0){
     echo "triple";
 }
+//this if checkes if the number is divisible to 5
 if ( $i % 5 == 0){
     echo "fiver";
 }
+//in case the number is divisible by both 3 and 5, the words "triple" and "fiver"
+//will be printed with no spabe between them, resulting in outputting triplefiver
 echo "\n";
 }
 
