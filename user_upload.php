bdb44b8cbf1b98743357bf316bce38892e56146e
o.zanganeh@gmail.com <ozangane@AD.MONASH.EDU>
2016-02-07 12:41:09 +1100

    Q1-reads the data from user.csv file and outputs the values. capitalizes the first letter of name and surname; and lowercase email addresses.

--- user_upload.php
+++ user_upload.php
@@ -1,15 +1,22 @@
 <?php
 
-//reading data from user.csv file
+//reads the data from user.csv file and outputs the values. capitalized the first letter of name and surname and lower case email addresses
 $row = 1;
 if (($handle = fopen("users.csv", "r")) !== FALSE) {
   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-    $num = count($data);
-    echo "$num fields in line $row: <br />\n";
+    $num = count($data); //variable num shows number of fields in each row.
     $row++;
+	if ( $row > 2){ //the first row of the file is heading ; starts showing the values from the seconf row
     for ($c=0; $c < $num; $c++) {
-		echo $data[$c] . "<br />\n";
+		if ($c < 2){ // email is in the 3rd field which needs to be validated. thus,, the if condition separates the first two cols. from the third
+		$data[$c] = ucfirst(strtolower($data[$c])); //strtolower function convers all the characters to lower case and ucfirst function capitalizes the first letter.
+		echo $data[$c]. "<br />\n";		
+		}else{
+			$data[2] = strtolower($data[2]);//as the question specifies, all the letters in email should be lowercase.
+			echo $data[2]. "<br />\n";
+		}
     }
+  }
   }
   fclose($handle);
 }
