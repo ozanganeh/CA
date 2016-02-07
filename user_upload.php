4960733d156eb1ec994e2c4a15c6cae66a8a297b
o.zanganeh@gmail.com <ozangane@AD.MONASH.EDU>
2016-02-07 12:54:14 +1100

    Q1-email validation added.

--- user_upload.php
+++ user_upload.php
@@ -8,16 +8,22 @@ if (($handle = fopen("users.csv", "r")) !== FALSE) {
     $row++;
 	if ( $row > 2){ //the first row of the file is heading ; starts showing the values from the seconf row
     for ($c=0; $c < $num; $c++) {
-		if ($c < 2){ // email is in the 3rd field which needs to be validated. thus,, the if condition separates the first two cols. from the third
-		$data[$c] = ucfirst(strtolower($data[$c])); //strtolower function convers all the characters to lower case and ucfirst function capitalizes the first letter.
+		if ($c < 2){// email is in the 3rd field which needs to be validated. thus,, the if condition separates the first two cols. from the third
+		$data[$c] = ucfirst(strtolower($data[$c]));//strtolower function convers all the characters to lower case and ucfirst function capitalizes the first letter.
 		echo $data[$c]. "<br />\n";		
 		}else{
-			$data[2] = strtolower($data[2]);//as the question specifies, all the letters in email should be lowercase.
+			if(filter_var($data[2], FILTER_VALIDATE_EMAIL)){ //check email validaity
+			$data[2] = strtolower($data[2]);
 			echo $data[2]. "<br />\n";
+			}else{
+				echo "Invalid email address <br />\n";
+				echo "this is the invalid email:" . $data[2]. " <br />\n";
+				$data[2] = NULL;
+			}
 		}
+	}
+	}
     }
-  }
-  }
   fclose($handle);
 }
 
